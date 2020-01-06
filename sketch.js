d02d93100ec69d7e9449a0da49fd1baa3fc2e50f
generative-exploration <59022519+generative-exploration@users.noreply.github.com>
2020-01-06 16:57:22 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 let img;
 let myFont;
 
-var zeile= 0;
+var zeile = 0;
 var positionY;
-var positionX; //für leichte seitliche verschiebung
+var positionX;
 var scanhoehe = 1;
 
 
-function preload(){ 
+function preload() {
   img = loadImage("0213.jpg");
   myFont = loadFont('FiraCode-Regular.ttf');
 }
@@ -21,40 +21,40 @@ function setup() {
 }
 
 function draw() {
-  copy(img, 0, height-20, width, height, 0, height-20, width, height);  
-  
-  if (zeile == 0){
+  copy(img, 0, height - 20, width, height, 0, height - 20, width, height);
+
+  if (zeile == 0) {
     positionY = mouseY;
-    positionX = int(map(mouseX, 0, width/2, -5, 5));
+    positionX = int(map(mouseX, 0, width / 2, -5, 5));
   }
 
-    if (mouseIsPressed) {
+  if (mouseIsPressed) {
     positionY = mouseY;
-    positionX = int(map(mouseX, 0, width/2, -5, 5));
-  } else { 
+    positionX = int(map(mouseX, 0, width / 2, -5, 5));
+  } else {
     positionY = zeile;
   }
-  
-  copy (img, 0, positionY, width, scanhoehe, 0-positionX, zeile, width-positionX, scanhoehe);
-  
-    zeile = zeile + scanhoehe;
-  
-   stroke('rgb(50%,50%,50%)');
-  line(40, zeile+2, width-40, zeile+2);
-  
-  if (zeile == height+50) {
+
+  copy(img, 0, positionY, width, scanhoehe, 0 - positionX, zeile, width - positionX, scanhoehe);
+
+  zeile = zeile + scanhoehe;
+
+  stroke('rgb(50%,50%,50%)');
+  line(40, zeile + 2, width - 40, zeile + 2);
+
+  if (zeile == height + 20) {
     zeile = 0;
+    image(img, 0, 0);
+
   }
-  
+
   fill(255);
   textAlign(CENTER);
-  text("scan: "+zeile , width/2, height-5);
+  text("scan: " + zeile, width / 2, height - 5);
 }
 
-function keyPressed() { 
-   if (key === 's') {
-      saveCanvas(c, 'gen_exploration', 'jpg');
+function keyPressed() {
+  if (key === 's') {
+    saveCanvas(c, 'gen_exploration', 'jpg');
   }
-  
 }
-
